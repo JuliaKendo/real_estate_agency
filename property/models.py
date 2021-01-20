a9b5cab640b3b037cb6243e6d1f00788755abf8c
Julia Kendo <ya1814@yandex.ru>
2021-01-20 20:12:16 +0300

    add field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 
+CHOICES = (
+    (None, "не заполнено"),
+    (True, "новостройка"),
+    (False, "старое здание")
+)
+
+
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    new_building = models.NullBooleanField(choices=CHOICES)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
 
     description = models.TextField("Текст объявления", blank=True)
