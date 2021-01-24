fa90e8f92b2514e000b4dc2b59d10cd6f71d36ae
Julia Kendo <ya1814@yandex.ru>
2021-01-25 01:04:09 +0300

    simplified code

--- property/migrations/0004_auto_20210120_2112.py
+++ property/migrations/0004_auto_20210120_2112.py
@@ -5,12 +5,7 @@ from django.db import migrations
 def fill_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        if flat.construction_year > 2015:
-            flat.new_building = True
-        elif flat.construction_year <= 2015:
-            flat.new_building = False
-        else:
-            continue
+        flat.new_building = flat.construction_year > 2015
         flat.save()
 
 
